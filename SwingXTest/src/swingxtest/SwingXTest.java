0955a61e171631d73dd5206222982b728d9ae2b7
javamaster <mauricio.ads10@gmail.com>
2013-12-12 10:52:43 -0200

    ok

---
+++ SwingXTest/src/swingxtest/SwingXTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swingxtest;
+
+/**
+ *
+ * @author Mauricio
+ */
+public class SwingXTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+//        int var = 0;
+                
+    }
+}
