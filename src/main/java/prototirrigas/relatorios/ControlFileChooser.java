0cab35ac967133aee5138214e03b50cf38c7689a
javamaster <mauricio.ads10@gmail.com>
2014-01-25 17:08:32 -0200

    Implementado FiileChooser para testes

---
+++ src/main/java/prototirrigas/relatorios/ControlFileChooser.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prototirrigas.relatorios;
+
+import javax.swing.JFileChooser;
+
+/**
+ *
+ * @author Mauricio
+ */
+public class ControlFileChooser {
+    
+    private static JFileChooser choosers;
+    
+    public static void main(String[] args) {
+        
+        JFileChooser chooser = new JFileChooser();
+        int retorno = chooser.showOpenDialog(null);
+        
+        if(retorno == JFileChooser.APPROVE_OPTION){
+              String path = chooser.getSelectedFile().getAbsolutePath();
+              System.out.println("Diretorio retornado: "+path);
+        }
+        else{
+        
+            System.out.println("Cancelou");
+        }
+    }
+    
+}
